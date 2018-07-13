149f1efee80750613055abece9a337c91c7d646f
SilentChaos512 <sworkman256@gmail.com>
2018-07-13 14:40:04 -0500

    Change crystal ore to new Lib class

--- src/main/java/net/silentchaos512/scalinghealth/init/ModBlocks.java
+++ src/main/java/net/silentchaos512/scalinghealth/init/ModBlocks.java
@@ -20,13 +20,26 @@ package net.silentchaos512.scalinghealth.init;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
+import net.silentchaos512.lib.block.BlockOreSL;
 import net.silentchaos512.lib.registry.IRegistrationHandler;
 import net.silentchaos512.lib.registry.SRegistry;
-import net.silentchaos512.scalinghealth.block.BlockCrystalOre;
+import net.silentchaos512.scalinghealth.config.Config;
+
+import java.util.Random;
 
 public class ModBlocks implements IRegistrationHandler<Block> {
 
-    public static BlockCrystalOre crystalOre = new BlockCrystalOre();
+    public static BlockOreSL crystalOre = new BlockOreSL(ModItems.crystalShard, 2, 1, 1, 1, 5) {
+        @Override
+        public int quantityDropped(Random random) {
+            return Config.HEART_CRYSTAL_ORE_QUANTITY_DROPPED;
+        }
+
+        @Override
+        public float bonusAmount(int fortune, Random random) {
+            return (fortune - 1) * random.nextFloat() - 1f;
+        }
+    };
 
     @Override
     public void registerAll(SRegistry reg) {
