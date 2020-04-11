45306e579e922d4c0d9b2cbe53cd2cf85dab2679
Cyborgmas <thpetuaudletang@gmail.com>
2020-04-11 14:33:05 -0400

    Messing with rendering + previous changelog and verison

--- src/main/java/net/silentchaos512/scalinghealth/client/render/entity/BlightFireRenderer.java
+++ src/main/java/net/silentchaos512/scalinghealth/client/render/entity/BlightFireRenderer.java
@@ -59,7 +59,6 @@ public final class BlightFireRenderer extends EntityRenderer<BlightFireEntity> {
             return false;
 
         AxisAlignedBB boundingBox = parent.getRenderBoundingBox().grow(0.5D);
-
         if (boundingBox.hasNaN() || boundingBox.getAverageEdgeLength() == 0.0D) {
             boundingBox = new AxisAlignedBB(
                     parent.getPosX() - 2.0D, parent.getPosY() - 2.0D, parent.getPosZ() - 2.0D,
@@ -74,28 +73,26 @@ public final class BlightFireRenderer extends EntityRenderer<BlightFireEntity> {
     public void render(BlightFireEntity fire, float entityYaw, float partialTicks, MatrixStack stack, IRenderTypeBuffer bufferIn, int light) {
         /*MobEntity parent = (MobEntity) fire.getRidingEntity();
         if (parent == null) return;
-        /*
+
         stack.push();
         stack.translate(fire.getPosX(), fire.getPosY() - parent.getHeight() + 0.5, fire.getPosZ());
-        float f = parent.getWidth() * FIRE_SCALE * 100f;
+        float f = parent.getWidth() * FIRE_SCALE;
         stack.scale(f, f, f);
 
-        IVertexBuilder vertexBuilder = bufferIn.getBuffer(RenderType.getTranslucent());
-
         float f1 = 0.5F;
-        float f2 = 0.0F;
-        float f3 = parent.getHeight() / f;
-        float f4 = (float) (parent.getPosY() - parent.getBoundingBox().minY);
+        float f2 = parent.getHeight() / f;
+        float f3 = (float) (parent.getPosY() - parent.getBoundingBox().minY);
+        float f4 = 0.0F;
 
-        stack.rotate(new Quaternion((float) -this.renderManager.info.getRenderViewEntity().getPosY(), 0.0F, 1.0F, 0.0F));
-        stack.translate(0, 0, f3 * 0.02f);
-
-        float f5 = 0.0F;
+        Quaternion rotation = new Quaternion(-this.renderManager.info.getYaw(), 0.0F, 1.0F, 0.0F);
+        stack.rotate(rotation);
+        stack.translate(0, 0, f2 * 0.02f);
         int i = 0;
 
         this.renderManager.textureManager.bindTexture(getEntityTexture(fire));
+        IVertexBuilder vertexBuilder = bufferIn.getBuffer(RenderType.getEntityGlint());
 
-        while (f3 > 0.0F) {
+        while (f2 > 0.0F) {
             boolean flag = i % 2 == 0;
             int frame = ClientTicks.ticksInGame() % 32;
             float minU = flag ? 0.5f : 0.0f;
@@ -104,26 +101,24 @@ public final class BlightFireRenderer extends EntityRenderer<BlightFireEntity> {
             float maxV = (frame + 1) / 32f;
 
             if (flag) {
-                float f10 = maxU;
+                float swap = maxU;
                 maxU = minU;
-                minU = f10;
+                minU = swap;
             }
 
             Matrix4f matrix4f = stack.getLast().getMatrix();
-            Matrix3f matrix3f = stack.getLast().getNormal();
-
-            vertexBuilder.pos(matrix4f,f1 - f2, 0.0F - f4, f5).color(1,1, 1, 1).tex(maxU, maxV).lightmap(light).normal(matrix3f, 1 ,0, -1).endVertex();
-            vertexBuilder.pos(matrix4f, -f1 - f2, 0.0F - f4, f5).color(1,1, 1, 1).tex(minU, maxV).lightmap(light).normal(matrix3f, 1 ,0, 1).endVertex();
-            vertexBuilder.pos(matrix4f,-f1 - f2, 1.4F - f4, f5).color(1,1, 1, 1).tex(minU, minV).lightmap(light).normal(matrix3f, -1 ,0, 1).endVertex();
-            vertexBuilder.pos(matrix4f, - f2, 1.4F - f4, f5).color(1,1, 1, 1).tex(maxU, minV).lightmap(light).normal(matrix3f, -1 ,0, 1).endVertex();
+            vertexBuilder.pos(matrix4f, f1,0.0F - f3, f4).tex(maxU, maxV).endVertex();
+            vertexBuilder.pos(matrix4f,-f1,0.0F - f3, f4).tex(minU, maxV).endVertex();
+            vertexBuilder.pos(matrix4f,-f1,1.4F - f3, f4).tex(maxU, minV).endVertex();
+            vertexBuilder.pos(matrix4f, f1,1.4F - f3, f4).tex(minU, minV).endVertex();
+            f2 -= 0.45F;
             f3 -= 0.45F;
-            f4 -= 0.45F;
             f1 *= 0.9F;
-            f5 += 0.03F;
+            f4 += 0.03F;
             ++i;
         }
         stack.pop();
-        */
+        super.render(fire, entityYaw, partialTicks, stack, bufferIn, light);*/
     }
 
     public static class Factory implements IRenderFactory<BlightFireEntity> {
